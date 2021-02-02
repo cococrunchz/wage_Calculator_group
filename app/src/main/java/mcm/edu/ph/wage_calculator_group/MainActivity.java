c152d526715890b6a334d796f2243890633f32ac
cococrunchz <76513885+cococrunchz@users.noreply.github.com>
2021-02-02 13:04:40 +0800

    added code

--- app/src/main/java/mcm/edu/ph/wage_calculator_group/MainActivity.java
+++ app/src/main/java/mcm/edu/ph/wage_calculator_group/MainActivity.java
@@ -3,12 +3,67 @@ package mcm.edu.ph.wage_calculator_group;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.zip.DeflaterOutputStream;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button calc;
+
+        calc = findViewById(R.id.btnCalc);
+
+        calc.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        EditText Hours;
+        TextView TotalSalary, HoursRendered, HoursOvertime, WageOvertime;
+
+        Hours = findViewById(R.id.txtHours);
+        TotalSalary = findViewById(R.id.txtTotalSalary);
+        HoursRendered = findViewById(R.id.txtHoursRendered);
+        WageOvertime = findViewById(R.id.txtWageOvertime);
+        HoursOvertime = findViewById(R.id.txtHoursOvertime);
+
+        Double hrs = 0.0;
+        Double totalsal = 0.0;
+        Double hrsrendered = 0.0;
+        Double wageovrtime = 0.0;
+        Double hrsovertime = 0.0;
+
+        hrs = Double.parseDouble(Hours.getText().toString());
+
+        if (hrs < 9 ){
+            totalsal = hrs * 100;
+            hrsrendered = hrs;
+
+            TotalSalary.setText(Double.toString(totalsal));
+            HoursRendered.setText(Double.toString(hrsrendered));
+        }
+        else if (hrs >= 9 ){
+            wageovrtime = hrs * 130;
+            hrsovertime = hrs;
+            WageOvertime.setText(Double.toString(wageovrtime));
+            HoursOvertime.setText(Double.toString(hrsovertime));
+        }
+
+
+
+
+
+
+
+
+
     }
 }
